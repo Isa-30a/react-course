d28646d3d117b80eb5b3666c23e2c70292bf7d9e
Isa-30a <isabella.alvasol@gmail.com>
2025-08-12 18:20:39 -0500

    refactor: simplify usePizzaOfTheDay test by using renderHook instead of a custom component

--- src/__test__/usePizzaOfTheDay.test.jsx
+++ src/__test__/usePizzaOfTheDay.test.jsx
@@ -1,5 +1,4 @@
 import { expect, test, vi } from "vitest";
-import { render } from "@testing-library/react";
 import createFetchMock from "vitest-fetch-mock";
 import { usePizzaOfTheday } from "../usePizzaOfTheDay";
 
@@ -15,34 +14,38 @@ const testPizza = {
   image: "/public/pizzas/calabrese.webp",
   sizes: { S: 12.25, M: 16.25, L: 20.25 },
 };
+/**import { render } from "@testing-library/react";
+ * //basically to test a hook you just want to create a component here to use the hook
+    function getPizzaOfTheDay() {
+    let pizza;
 
-function getPizzaOfTheDay() {
-  let pizza;
+    function TestComponent() {
+        //you dont want to render anything you just want to get whatever pizza have
+        pizza = usePizzaOfTheday();
+        return null;
+    }
 
-  function TestComponent() {
-    pizza = usePizzaOfTheday();
-    return null;
-  }
+    render(<TestComponent />);
 
-  render(<TestComponent />);
+    return pizza;
+    }
+    test("gives undefined when first called", async () => {
+    fetch.mockResponseOnce(JSON.stringify(testPizza));
+    const pizza = getPizzaOfTheDay();
+    expect(pizza).toBeUndefined();
+    });
+ */
 
-  return pizza;
-}
-
-test("gives null when first called", async () => {
-  fetch.mockResponseOnce(JSON.stringify(testPizza));
-  const pizza = getPizzaOfTheDay();
-  expect(pizza).toBeUndefined();
-});
 // It's a little weird to implement a fake component to test something (we're dangerously close to the line of testing
 // implementation details) but this is essentially library code and we want to assure ourselves this code works if we use
 //  it frequently in our code base.
 // We can make this better though. Let's rewrite our test to look like this:
 
-// import { renderHook } from "@testing-library/react"; // change import
+import { renderHook } from "@testing-library/react"; // change import
 
-// test("to be null on initial load", async () => {
-//   fetch.mockResponseOnce(JSON.stringify(testPizza));
-//   const { result } = renderHook(() => usePizzaOfTheDay(""));
-//   expect(result.current).toBeNull();
-// });
+test("to be null on initial load", async () => {
+  fetch.mockResponseOnce(JSON.stringify(testPizza));
+  // Instead of creating a custom component, simply pass the hook to renderHook for a concise test.
+  const { result } = renderHook(() => usePizzaOfTheday());
+  expect(result.current).toBeUndefined();
+});
